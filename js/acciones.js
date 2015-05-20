149af6a59d1e0cdcec8bafa19ef1335f3ed43e4b
AL13F13 <dsauposafa@gmail.com>
2015-05-20 13:24:26 -0500

    uno
    los divs tienes colores que envian numeros al footer

---
+++ js/acciones.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+$(document).ready(function(e) {
+//document.addEventListener("deviceready",function(){
+$('#1').on('click', function(){
+$('#resultado').append('1,');
+});
+$('#2').on('click', function(){
+$('#resultado').append('2,');
+});
+$('#3').on('click', function(){
+$('#resultado').append('3,');
+});
+$('#4').on('click', function(){
+$('#resultado').append('4,');
+});
+	
+	
+//}); 
+});
+	
+ 	
+
+
+
